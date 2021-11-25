ccaeef9f1aa6052ec4a08c019df1357d9a0d2ea0
hanhan207 <785280596@qq.com>
2021-11-25 16:15:34 +0800

    fonts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import  Texty from 'rc-texty';
+import ScrollAnim from 'rc-scroll-anim';
+const ScrollOverPack = ScrollAnim.OverPack;
 
 function App() {
+  const windowheight = document.documentElement.clientHeight
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <ScrollOverPack className='container'  style={{height:windowheight*0.6}}>
+      <Texty className='titleP0'>How many time have we met this year?</Texty>
+      </ScrollOverPack>
+      <ScrollOverPack className='container'  style={{height:windowheight*0.6}}>
+      <Texty className='titleP0'>How many time have we met this year?</Texty>
+      </ScrollOverPack>
+      <ScrollOverPack className='container'  style={{height:windowheight*0.6}}>
+      <Texty className='titleP0'>How many time have we met this year?</Texty>
+      </ScrollOverPack>
+      <ScrollOverPack className='container'  style={{height:windowheight*0.6}}>
+      <Texty className='titleP0'>How many time have we met this year?</Texty>
+      </ScrollOverPack>
+    
     </div>
   );
 }
