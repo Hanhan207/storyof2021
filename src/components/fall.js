705ee5fa9793c3b2527df11e5c55fe06a9d33519
hanhan207 <785280596@qq.com>
2021-11-30 17:56:15 +0800

    pie

--- src/components/fall.js
+++ src/components/fall.js
@@ -21,15 +21,10 @@ function Fall(){
         count:35
     },
 ]
-const data = [
-    { type: '分类一', value: 27 },
-    { type: '分类二', value: 25 },
-    { type: '分类三', value: 18 },
-    { type: '分类四', value: 15 },
-    { type: '分类五', value: 10 },
-    { type: '其他', value: 5 },
-  ];
 
+function getcontent(e){
+  console.log('e',e)
+}
 useEffect(() => {
     const piePlot = new Pie('pie', {
         appendPadding: 10,
@@ -43,7 +38,7 @@ useEffect(() => {
         label: {
           type: 'inner',
           offset: '-8%',
-        //   content: '{name}',
+          content: '{value}次',
           style: { fontSize: 18 },
         },
         interactions: [{ type: 'element-active' }],
